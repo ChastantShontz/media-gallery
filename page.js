b20cd4e5f8bff3b7756e3968e7236d6d527cf91e
ChastantShontz <78157527+ChastantShontz@users.noreply.github.com>
2021-10-27 14:10:53 -0400

    View/hide layer
    Made the view functions add the class .layerTrans to the div .layer.
    
    Made the close functions remove the class .layerTrans from the div. layer.

--- page.js
+++ page.js
@@ -1,167 +1,215 @@
 function haazenView() {
-  var view = document.getElementById('haazenOrig');
-  view.classList.add('charTrans');
-  var view = document.getElementById('haazenNew');
-  view.classList.add('charHidTrans');
+  var x = document.getElementById('haazen');
+  x.classList.add('charTrans');
+  var x = document.getElementById('haazenHid');
+  x.classList.add('charHidTrans');
+  var x = document.getElementById('layer');
+  x.classList.add('layerTrans');
 }
 
 function haazenClose() {
-  var view = document.getElementById('haazenOrig');
-  view.classList.remove('charTrans');
-  var view = document.getElementById('haazenNew');
-  view.classList.remove('charHidTrans');
+  var x = document.getElementById('haazen');
+  x.classList.remove('charTrans');
+  var x = document.getElementById('haazenHid');
+  x.classList.remove('charHidTrans');
+  var x = document.getElementById('layer');
+  x.classList.remove('layerTrans');
 }
 
 function darthTrayaView() {
-  var view = document.getElementById('darthTrayaOrig');
-  view.classList.add('charTrans');
-  var view = document.getElementById('darthTrayaNew');
-  view.classList.add('charHidTrans');
+  var x = document.getElementById('darthTraya');
+  x.classList.add('charTrans');
+  var x = document.getElementById('darthTrayaHid');
+  x.classList.add('charHidTrans');
+  var x = document.getElementById('layer');
+  x.classList.add('layerTrans');
 }
 
 function darthTrayaClose() {
-  var view = document.getElementById('darthTrayaOrig');
-  view.classList.remove('charTrans');
-  var view = document.getElementById('darthTrayaNew');
-  view.classList.remove('charHidTrans');
+  var x = document.getElementById('darthTraya');
+  x.classList.remove('charTrans');
+  var x = document.getElementById('darthTrayaHid');
+  x.classList.remove('charHidTrans');
+  var x = document.getElementById('layer');
+  x.classList.remove('layerTrans');
 }
 
 function revanView() {
-  var view = document.getElementById('revanOrig');
-  view.classList.add('charTrans');
-  var view = document.getElementById('revanNew');
-  view.classList.add('charHidTrans');
+  var x = document.getElementById('revan');
+  x.classList.add('charTrans');
+  var x = document.getElementById('revanHid');
+  x.classList.add('charHidTrans');
+  var x = document.getElementById('layer');
+  x.classList.add('layerTrans');
 }
 
 function revanClose() {
-  var view = document.getElementById('revanOrig');
-  view.classList.remove('charTrans');
-  var view = document.getElementById('revanNew');
-  view.classList.remove('charHidTrans');
+  var x = document.getElementById('revan');
+  x.classList.remove('charTrans');
+  var x = document.getElementById('revanHid');
+  x.classList.remove('charHidTrans');
+  var x = document.getElementById('layer');
+  x.classList.remove('layerTrans');
 }
 
 function meetraSurikView() {
-  var view = document.getElementById('meetraSurikOrig');
-  view.classList.add('charTrans');
-  var view = document.getElementById('meetraSurikNew');
-  view.classList.add('charHidTrans');
+  var x = document.getElementById('meetraSurik');
+  x.classList.add('charTrans');
+  var x = document.getElementById('meetraSurikHid');
+  x.classList.add('charHidTrans');
+  var x = document.getElementById('layer');
+  x.classList.add('layerTrans');
 }
 
 function meetraSurikClose() {
-  var view = document.getElementById('meetraSurikOrig');
-  view.classList.remove('charTrans');
-  var view = document.getElementById('meetraSurikNew');
-  view.classList.remove('charHidTrans');
+  var x = document.getElementById('meetraSurik');
+  x.classList.remove('charTrans');
+  var x = document.getElementById('meetraSurikHid');
+  x.classList.remove('charHidTrans');
+  var x = document.getElementById('layer');
+  x.classList.remove('layerTrans');
 }
 
 function darthBaneView() {
-  var view = document.getElementById('darthBaneOrig');
-  view.classList.add('charTrans');
-  var view = document.getElementById('darthBaneNew');
-  view.classList.add('charHidTrans');
+  var x = document.getElementById('darthBane');
+  x.classList.add('charTrans');
+  var x = document.getElementById('darthBaneHid');
+  x.classList.add('charHidTrans');
+  var x = document.getElementById('layer');
+  x.classList.add('layerTrans');
 }
 
 function darthBaneClose() {
-  var view = document.getElementById('darthBaneOrig');
-  view.classList.remove('charTrans');
-  var view = document.getElementById('darthBaneNew');
-  view.classList.remove('charHidTrans');
+  var x = document.getElementById('darthBane');
+  x.classList.remove('charTrans');
+  var x = document.getElementById('darthBaneHid');
+  x.classList.remove('charHidTrans');
+  var x = document.getElementById('layer');
+  x.classList.remove('layerTrans');
 }
 
 function quinlanVosView() {
-  var view = document.getElementById('quinlanVosOrig');
-  view.classList.add('charTrans');
-  var view = document.getElementById('quinlanVosNew');
-  view.classList.add('charHidTrans');
+  var x = document.getElementById('quinlanVos');
+  x.classList.add('charTrans');
+  var x = document.getElementById('quinlanVosHid');
+  x.classList.add('charHidTrans');
+  var x = document.getElementById('layer');
+  x.classList.add('layerTrans');
 }
 
 function quinlanVosClose() {
-  var view = document.getElementById('quinlanVosOrig');
-  view.classList.remove('charTrans');
-  var view = document.getElementById('quinlanVosNew');
-  view.classList.remove('charHidTrans');
+  var x = document.getElementById('quinlanVos');
+  x.classList.remove('charTrans');
+  var x = document.getElementById('quinlanVosHid');
+  x.classList.remove('charHidTrans');
+  var x = document.getElementById('layer');
+  x.classList.remove('layerTrans');
 }
 
 function darthKraytView() {
-  var view = document.getElementById('darthKraytOrig');
-  view.classList.add('charTrans');
-  var view = document.getElementById('darthKraytNew');
-  view.classList.add('charHidTrans');
+  var x = document.getElementById('darthKrayt');
+  x.classList.add('charTrans');
+  var x = document.getElementById('darthKraytHid');
+  x.classList.add('charHidTrans');
+  var x = document.getElementById('layer');
+  x.classList.add('layerTrans');
 }
 
 function darthKraytClose() {
-  var view = document.getElementById('darthKraytOrig');
-  view.classList.remove('charTrans');
-  var view = document.getElementById('darthKraytNew');
-  view.classList.remove('charHidTrans');
+  var x = document.getElementById('darthKrayt');
+  x.classList.remove('charTrans');
+  var x = document.getElementById('darthKraytHid');
+  x.classList.remove('charHidTrans');
+  var x = document.getElementById('layer');
+  x.classList.remove('layerTrans');
 }
 
 function grandAdmiralThrawnView() {
-  var view = document.getElementById('grandAdmiralThrawnOrig');
-  view.classList.add('charTrans');
-  var view = document.getElementById('grandAdmiralThrawnNew');
-  view.classList.add('charHidTrans');
+  var x = document.getElementById('grandAdmiralThrawn');
+  x.classList.add('charTrans');
+  var x = document.getElementById('grandAdmiralThrawnHid');
+  x.classList.add('charHidTrans');
+  var x = document.getElementById('layer');
+  x.classList.add('layerTrans');
 }
 
 function grandAdmiralThrawnClose() {
-  var view = document.getElementById('grandAdmiralThrawnOrig');
-  view.classList.remove('charTrans');
-  var view = document.getElementById('grandAdmiralThrawnNew');
-  view.classList.remove('charHidTrans');
+  var x = document.getElementById('grandAdmiralThrawn');
+  x.classList.remove('charTrans');
+  var x = document.getElementById('grandAdmiralThrawnHid');
+  x.classList.remove('charHidTrans');
+  var x = document.getElementById('layer');
+  x.classList.remove('layerTrans');
 }
 
 function lumiyaView() {
-  var view = document.getElementById('lumiyaOrig');
-  view.classList.add('charTrans');
-  var view = document.getElementById('lumiyaNew');
-  view.classList.add('charHidTrans');
+  var x = document.getElementById('lumiya');
+  x.classList.add('charTrans');
+  var x = document.getElementById('lumiyaHid');
+  x.classList.add('charHidTrans');
+  var x = document.getElementById('layer');
+  x.classList.add('layerTrans');
 }
 
 function lumiyaClose() {
-  var view = document.getElementById('lumiyaOrig');
-  view.classList.remove('charTrans');
-  var view = document.getElementById('lumiyaNew');
-  view.classList.remove('charHidTrans');
+  var x = document.getElementById('lumiya');
+  x.classList.remove('charTrans');
+  var x = document.getElementById('lumiyaHid');
+  x.classList.remove('charHidTrans');
+  var x = document.getElementById('layer');
+  x.classList.remove('layerTrans');
 }
 
 function jacenSoloView() {
-  var view = document.getElementById('jacenSoloOrig');
-  view.classList.add('charTrans');
-  var view = document.getElementById('jacenSoloNew');
-  view.classList.add('charHidTrans');
+  var x = document.getElementById('jacenSolo');
+  x.classList.add('charTrans');
+  var x = document.getElementById('jacenSoloHid');
+  x.classList.add('charHidTrans');
+  var x = document.getElementById('layer');
+  x.classList.add('layerTrans');
 }
 
 function jacenSoloClose() {
-  var view = document.getElementById('jacenSoloOrig');
-  view.classList.remove('charTrans');
-  var view = document.getElementById('jacenSoloNew');
-  view.classList.remove('charHidTrans');
+  var x = document.getElementById('jacenSolo');
+  x.classList.remove('charTrans');
+  var x = document.getElementById('jacenSoloHid');
+  x.classList.remove('charHidTrans');
+  var x = document.getElementById('layer');
+  x.classList.remove('layerTrans');
 }
 
 function morriganCordeView() {
-  var view = document.getElementById('morriganCordeOrig');
-  view.classList.add('charTrans');
-  var view = document.getElementById('morriganCordeNew');
-  view.classList.add('charHidTrans');
+  var x = document.getElementById('morriganCorde');
+  x.classList.add('charTrans');
+  var x = document.getElementById('morriganCordeHid');
+  x.classList.add('charHidTrans');
+  var x = document.getElementById('layer');
+  x.classList.add('layerTrans');
 }
 
 function morriganCordeClose() {
-  var view = document.getElementById('morriganCordeOrig');
-  view.classList.remove('charTrans');
-  var view = document.getElementById('morriganCordeNew');
-  view.classList.remove('charHidTrans');
+  var x = document.getElementById('morriganCorde');
+  x.classList.remove('charTrans');
+  var x = document.getElementById('morriganCordeHid');
+  x.classList.remove('charHidTrans');
+  var x = document.getElementById('layer');
+  x.classList.remove('layerTrans');
 }
 
 function cadeSkywalkerView() {
-  var view = document.getElementById('cadeSkywalkerOrig');
-  view.classList.add('charTrans');
-  var view = document.getElementById('cadeSkywalkerNew');
-  view.classList.add('charHidTrans');
+  var x = document.getElementById('cadeSkywalker');
+  x.classList.add('charTrans');
+  var x = document.getElementById('cadeSkywalkerHid');
+  x.classList.add('charHidTrans');
+  var x = document.getElementById('layer');
+  x.classList.add('layerTrans');
 }
 
 function cadeSkywalkerClose() {
-  var view = document.getElementById('cadeSkywalkerOrig');
-  view.classList.remove('charTrans');
-  var view = document.getElementById('cadeSkywalkerNew');
-  view.classList.remove('charHidTrans');
+  var x = document.getElementById('cadeSkywalker');
+  x.classList.remove('charTrans');
+  var x = document.getElementById('cadeSkywalkerHid');
+  x.classList.remove('charHidTrans');
+  var x = document.getElementById('layer');
+  x.classList.remove('layerTrans');
 }
