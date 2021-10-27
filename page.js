0c51b336608d65f29a7902eb15652d647296f4af
ChastantShontz <78157527+ChastantShontz@users.noreply.github.com>
2021-10-27 13:25:02 -0400

    View/hide divs
    Added an onclick function to each view button.
    
    Added IDs to each .char and .charHid div so they can be identified with JavaScript.
    
    Wrote a JavaScript function that adds the class .charTrans to .char and adds .charHidTrans to .charHid.

--- page.js
+++ page.js
@@ -1,4 +1,83 @@
-function change() {
-  var title = document.getElementById('test');
-  title.classList.add('testTrans');
+function haazenView() {
+  var view = document.getElementById('haazenOrig');
+  view.classList.add('charTrans');
+  var view = document.getElementById('haazenNew');
+  view.classList.add('charHidTrans');
+}
+
+function darthTrayaView() {
+  var view = document.getElementById('darthTrayaOrig');
+  view.classList.add('charTrans');
+  var view = document.getElementById('darthTrayaNew');
+  view.classList.add('charHidTrans');
+}
+
+function revanView() {
+  var view = document.getElementById('revanOrig');
+  view.classList.add('charTrans');
+  var view = document.getElementById('revanNew');
+  view.classList.add('charHidTrans');
+}
+
+function meetraSurikView() {
+  var view = document.getElementById('meetraSurikOrig');
+  view.classList.add('charTrans');
+  var view = document.getElementById('meetraSurikNew');
+  view.classList.add('charHidTrans');
+}
+
+function darthBaneView() {
+  var view = document.getElementById('darthBaneOrig');
+  view.classList.add('charTrans');
+  var view = document.getElementById('darthBaneNew');
+  view.classList.add('charHidTrans');
+}
+
+function quinlanVosView() {
+  var view = document.getElementById('quinlanVosOrig');
+  view.classList.add('charTrans');
+  var view = document.getElementById('quinlanVosNew');
+  view.classList.add('charHidTrans');
+}
+
+function darthKraytView() {
+  var view = document.getElementById('darthKraytOrig');
+  view.classList.add('charTrans');
+  var view = document.getElementById('darthKraytNew');
+  view.classList.add('charHidTrans');
+}
+
+function grandAdmiralThrawnView() {
+  var view = document.getElementById('grandAdmiralThrawnOrig');
+  view.classList.add('charTrans');
+  var view = document.getElementById('grandAdmiralThrawnNew');
+  view.classList.add('charHidTrans');
+}
+
+function lumiyaView() {
+  var view = document.getElementById('lumiyaOrig');
+  view.classList.add('charTrans');
+  var view = document.getElementById('lumiyaNew');
+  view.classList.add('charHidTrans');
+}
+
+function jacenSoloView() {
+  var view = document.getElementById('jacenSoloOrig');
+  view.classList.add('charTrans');
+  var view = document.getElementById('jacenSoloNew');
+  view.classList.add('charHidTrans');
+}
+
+function morriganCordeView() {
+  var view = document.getElementById('morriganCordeOrig');
+  view.classList.add('charTrans');
+  var view = document.getElementById('morriganCordeNew');
+  view.classList.add('charHidTrans');
+}
+
+function cadeSkywalkerView() {
+  var view = document.getElementById('cadeSkywalkerOrig');
+  view.classList.add('charTrans');
+  var view = document.getElementById('cadeSkywalkerNew');
+  view.classList.add('charHidTrans');
 }
