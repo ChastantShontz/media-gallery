c3fbed6d595896665d8bb16d7269d70f88c6c010
ChastantShontz <78157527+ChastantShontz@users.noreply.github.com>
2021-10-27 13:34:54 -0400

    Close divs
    Added an onclick function to each close button.
    
    Wrote a JavaScript function that removes the class .charTrans from .char and removes .charHidTrans from .charHid.

--- page.js
+++ page.js
@@ -5,11 +5,25 @@ function haazenView() {
   view.classList.add('charHidTrans');
 }
 
+function haazenClose() {
+  var view = document.getElementById('haazenOrig');
+  view.classList.remove('charTrans');
+  var view = document.getElementById('haazenNew');
+  view.classList.remove('charHidTrans');
+}
+
 function darthTrayaView() {
   var view = document.getElementById('darthTrayaOrig');
   view.classList.add('charTrans');
   var view = document.getElementById('darthTrayaNew');
   view.classList.add('charHidTrans');
+}
+
+function darthTrayaClose() {
+  var view = document.getElementById('darthTrayaOrig');
+  view.classList.remove('charTrans');
+  var view = document.getElementById('darthTrayaNew');
+  view.classList.remove('charHidTrans');
 }
 
 function revanView() {
@@ -19,11 +33,25 @@ function revanView() {
   view.classList.add('charHidTrans');
 }
 
+function revanClose() {
+  var view = document.getElementById('revanOrig');
+  view.classList.remove('charTrans');
+  var view = document.getElementById('revanNew');
+  view.classList.remove('charHidTrans');
+}
+
 function meetraSurikView() {
   var view = document.getElementById('meetraSurikOrig');
   view.classList.add('charTrans');
   var view = document.getElementById('meetraSurikNew');
   view.classList.add('charHidTrans');
+}
+
+function meetraSurikClose() {
+  var view = document.getElementById('meetraSurikOrig');
+  view.classList.remove('charTrans');
+  var view = document.getElementById('meetraSurikNew');
+  view.classList.remove('charHidTrans');
 }
 
 function darthBaneView() {
@@ -33,11 +61,25 @@ function darthBaneView() {
   view.classList.add('charHidTrans');
 }
 
+function darthBaneClose() {
+  var view = document.getElementById('darthBaneOrig');
+  view.classList.remove('charTrans');
+  var view = document.getElementById('darthBaneNew');
+  view.classList.remove('charHidTrans');
+}
+
 function quinlanVosView() {
   var view = document.getElementById('quinlanVosOrig');
   view.classList.add('charTrans');
   var view = document.getElementById('quinlanVosNew');
   view.classList.add('charHidTrans');
+}
+
+function quinlanVosClose() {
+  var view = document.getElementById('quinlanVosOrig');
+  view.classList.remove('charTrans');
+  var view = document.getElementById('quinlanVosNew');
+  view.classList.remove('charHidTrans');
 }
 
 function darthKraytView() {
@@ -47,11 +89,25 @@ function darthKraytView() {
   view.classList.add('charHidTrans');
 }
 
+function darthKraytClose() {
+  var view = document.getElementById('darthKraytOrig');
+  view.classList.remove('charTrans');
+  var view = document.getElementById('darthKraytNew');
+  view.classList.remove('charHidTrans');
+}
+
 function grandAdmiralThrawnView() {
   var view = document.getElementById('grandAdmiralThrawnOrig');
   view.classList.add('charTrans');
   var view = document.getElementById('grandAdmiralThrawnNew');
   view.classList.add('charHidTrans');
+}
+
+function grandAdmiralThrawnClose() {
+  var view = document.getElementById('grandAdmiralThrawnOrig');
+  view.classList.remove('charTrans');
+  var view = document.getElementById('grandAdmiralThrawnNew');
+  view.classList.remove('charHidTrans');
 }
 
 function lumiyaView() {
@@ -61,11 +117,25 @@ function lumiyaView() {
   view.classList.add('charHidTrans');
 }
 
+function lumiyaClose() {
+  var view = document.getElementById('lumiyaOrig');
+  view.classList.remove('charTrans');
+  var view = document.getElementById('lumiyaNew');
+  view.classList.remove('charHidTrans');
+}
+
 function jacenSoloView() {
   var view = document.getElementById('jacenSoloOrig');
   view.classList.add('charTrans');
   var view = document.getElementById('jacenSoloNew');
   view.classList.add('charHidTrans');
+}
+
+function jacenSoloClose() {
+  var view = document.getElementById('jacenSoloOrig');
+  view.classList.remove('charTrans');
+  var view = document.getElementById('jacenSoloNew');
+  view.classList.remove('charHidTrans');
 }
 
 function morriganCordeView() {
@@ -75,9 +145,23 @@ function morriganCordeView() {
   view.classList.add('charHidTrans');
 }
 
+function morriganCordeClose() {
+  var view = document.getElementById('morriganCordeOrig');
+  view.classList.remove('charTrans');
+  var view = document.getElementById('morriganCordeNew');
+  view.classList.remove('charHidTrans');
+}
+
 function cadeSkywalkerView() {
   var view = document.getElementById('cadeSkywalkerOrig');
   view.classList.add('charTrans');
   var view = document.getElementById('cadeSkywalkerNew');
   view.classList.add('charHidTrans');
+}
+
+function cadeSkywalkerClose() {
+  var view = document.getElementById('cadeSkywalkerOrig');
+  view.classList.remove('charTrans');
+  var view = document.getElementById('cadeSkywalkerNew');
+  view.classList.remove('charHidTrans');
 }
