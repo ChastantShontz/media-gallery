061a970f3ba92c8bb69f4ae1a3d701da88e41c8b
Chastant Shontz <shontzcp@clarkson.edu>
2024-01-29 02:05:08 -0500

    Initial commit

---
+++ page.js
@@ -0,0 +1,266 @@
+function haazenView() {
+  var x = document.getElementById('haazen');
+  x.classList.add('charTrans');
+  var x = document.getElementById('haazenHid');
+  x.classList.add('charHidTrans');
+  var x = document.getElementById('layer');
+  x.classList.add('layerTrans');
+}
+
+function haazenClose() {
+  var x = document.getElementById('haazen');
+  x.classList.remove('charTrans');
+  var x = document.getElementById('haazenHid');
+  x.classList.remove('charHidTrans');
+  var x = document.getElementById('layer');
+  x.classList.remove('layerTrans');
+}
+
+function darthTrayaView() {
+  var x = document.getElementById('darthTraya');
+  x.classList.add('charTrans');
+  var x = document.getElementById('darthTrayaHid');
+  x.classList.add('charHidTrans');
+  var x = document.getElementById('layer');
+  x.classList.add('layerTrans');
+}
+
+function darthTrayaClose() {
+  var x = document.getElementById('darthTraya');
+  x.classList.remove('charTrans');
+  var x = document.getElementById('darthTrayaHid');
+  x.classList.remove('charHidTrans');
+  var x = document.getElementById('layer');
+  x.classList.remove('layerTrans');
+}
+
+function revanView() {
+  var x = document.getElementById('revan');
+  x.classList.add('charTrans');
+  var x = document.getElementById('revanHid');
+  x.classList.add('charHidTrans');
+  var x = document.getElementById('layer');
+  x.classList.add('layerTrans');
+}
+
+function revanClose() {
+  var x = document.getElementById('revan');
+  x.classList.remove('charTrans');
+  var x = document.getElementById('revanHid');
+  x.classList.remove('charHidTrans');
+  var x = document.getElementById('layer');
+  x.classList.remove('layerTrans');
+}
+
+function meetraSurikView() {
+  var x = document.getElementById('meetraSurik');
+  x.classList.add('charTrans');
+  var x = document.getElementById('meetraSurikHid');
+  x.classList.add('charHidTrans');
+  var x = document.getElementById('layer');
+  x.classList.add('layerTrans');
+}
+
+function meetraSurikClose() {
+  var x = document.getElementById('meetraSurik');
+  x.classList.remove('charTrans');
+  var x = document.getElementById('meetraSurikHid');
+  x.classList.remove('charHidTrans');
+  var x = document.getElementById('layer');
+  x.classList.remove('layerTrans');
+}
+
+function darthBaneView() {
+  var x = document.getElementById('darthBane');
+  x.classList.add('charTrans');
+  var x = document.getElementById('darthBaneHid');
+  x.classList.add('charHidTrans');
+  var x = document.getElementById('layer');
+  x.classList.add('layerTrans');
+}
+
+function darthBaneClose() {
+  var x = document.getElementById('darthBane');
+  x.classList.remove('charTrans');
+  var x = document.getElementById('darthBaneHid');
+  x.classList.remove('charHidTrans');
+  var x = document.getElementById('layer');
+  x.classList.remove('layerTrans');
+}
+
+function quinlanVosView() {
+  var x = document.getElementById('quinlanVos');
+  x.classList.add('charTrans');
+  var x = document.getElementById('quinlanVosHid');
+  x.classList.add('charHidTrans');
+  var x = document.getElementById('layer');
+  x.classList.add('layerTrans');
+}
+
+function quinlanVosClose() {
+  var x = document.getElementById('quinlanVos');
+  x.classList.remove('charTrans');
+  var x = document.getElementById('quinlanVosHid');
+  x.classList.remove('charHidTrans');
+  var x = document.getElementById('layer');
+  x.classList.remove('layerTrans');
+}
+
+function darthKraytView() {
+  var x = document.getElementById('darthKrayt');
+  x.classList.add('charTrans');
+  var x = document.getElementById('darthKraytHid');
+  x.classList.add('charHidTrans');
+  var x = document.getElementById('layer');
+  x.classList.add('layerTrans');
+}
+
+function darthKraytClose() {
+  var x = document.getElementById('darthKrayt');
+  x.classList.remove('charTrans');
+  var x = document.getElementById('darthKraytHid');
+  x.classList.remove('charHidTrans');
+  var x = document.getElementById('layer');
+  x.classList.remove('layerTrans');
+}
+
+function grandAdmiralThrawnView() {
+  var x = document.getElementById('grandAdmiralThrawn');
+  x.classList.add('charTrans');
+  var x = document.getElementById('grandAdmiralThrawnHid');
+  x.classList.add('charHidTrans');
+  var x = document.getElementById('layer');
+  x.classList.add('layerTrans');
+}
+
+function grandAdmiralThrawnClose() {
+  var x = document.getElementById('grandAdmiralThrawn');
+  x.classList.remove('charTrans');
+  var x = document.getElementById('grandAdmiralThrawnHid');
+  x.classList.remove('charHidTrans');
+  var x = document.getElementById('layer');
+  x.classList.remove('layerTrans');
+}
+
+function lumiyaView() {
+  var x = document.getElementById('lumiya');
+  x.classList.add('charTrans');
+  var x = document.getElementById('lumiyaHid');
+  x.classList.add('charHidTrans');
+  var x = document.getElementById('layer');
+  x.classList.add('layerTrans');
+}
+
+function lumiyaClose() {
+  var x = document.getElementById('lumiya');
+  x.classList.remove('charTrans');
+  var x = document.getElementById('lumiyaHid');
+  x.classList.remove('charHidTrans');
+  var x = document.getElementById('layer');
+  x.classList.remove('layerTrans');
+}
+
+function jacenSoloView() {
+  var x = document.getElementById('jacenSolo');
+  x.classList.add('charTrans');
+  var x = document.getElementById('jacenSoloHid');
+  x.classList.add('charHidTrans');
+  var x = document.getElementById('layer');
+  x.classList.add('layerTrans');
+}
+
+function jacenSoloClose() {
+  var x = document.getElementById('jacenSolo');
+  x.classList.remove('charTrans');
+  var x = document.getElementById('jacenSoloHid');
+  x.classList.remove('charHidTrans');
+  var x = document.getElementById('layer');
+  x.classList.remove('layerTrans');
+}
+
+function morriganCordeView() {
+  var x = document.getElementById('morriganCorde');
+  x.classList.add('charTrans');
+  var x = document.getElementById('morriganCordeHid');
+  x.classList.add('charHidTrans');
+  var x = document.getElementById('layer');
+  x.classList.add('layerTrans');
+}
+
+function morriganCordeClose() {
+  var x = document.getElementById('morriganCorde');
+  x.classList.remove('charTrans');
+  var x = document.getElementById('morriganCordeHid');
+  x.classList.remove('charHidTrans');
+  var x = document.getElementById('layer');
+  x.classList.remove('layerTrans');
+}
+
+function cadeSkywalkerView() {
+  var x = document.getElementById('cadeSkywalker');
+  x.classList.add('charTrans');
+  var x = document.getElementById('cadeSkywalkerHid');
+  x.classList.add('charHidTrans');
+  var x = document.getElementById('layer');
+  x.classList.add('layerTrans');
+}
+
+function cadeSkywalkerClose() {
+  var x = document.getElementById('cadeSkywalker');
+  x.classList.remove('charTrans');
+  var x = document.getElementById('cadeSkywalkerHid');
+  x.classList.remove('charHidTrans');
+  var x = document.getElementById('layer');
+  x.classList.remove('layerTrans');
+}
+
+function uncover() {
+  var x = document.getElementsByClassName('char');
+  var y;
+  for (y = 0; y < x.length; y++) {
+    x[y].classList.remove('charTrans');
+  }
+  var x = document.getElementsByClassName('charHid');
+  var y;
+  for (y = 0; y < x.length; y++) {
+    x[y].classList.remove('charHidTrans');
+  }
+  var x = document.getElementById('layer');
+  x.classList.remove('layerTrans');
+}
+
+var x = 1;
+showSlide(x);
+
+function changeSlide(n) {
+  showSlide(x += n);
+}
+
+function currentSlide(n) {
+  showSlide(x = n);
+}
+
+function showSlide(n) {
+  var y;
+  var slide = document.getElementsByClassName("primEvent");
+  if (n > slide.length) {
+    x = 1;
+  }
+  if (n < 1) {
+    x = slide.length;
+  }
+  for (y = 0; y < slide.length; y++) {
+    slide[y].style.display = "none";
+  }
+  slide[x - 1].style.display = "block";
+  var preview = document.getElementsByClassName("event");
+  for (y = 0; y < preview.length; y++) {
+    preview[y].style.opacity = "50%";
+  }
+  preview[x - 1].style.opacity = "100%";
+  var label = document.getElementsByClassName("eventLabel");
+  for (y = 0; y < label.length; y++) {
+    label[y].style.background = "linear-gradient(to right, rgba(255, 255, 255, 0%), 20%, var(--gray), 80%, rgba(255, 255, 255, 0%))";
+  }
+  label[x - 1].style.background = "var(--gray)";
+}
